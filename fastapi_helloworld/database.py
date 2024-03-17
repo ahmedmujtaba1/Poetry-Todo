a980d054eaa6e1819a712889dc420d2500ea92a0
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2024-03-17 21:34:53 +0500

    All Done

--- fastapi_helloworld/database.py
+++ fastapi_helloworld/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ahmedmujtaba1:1bKOzMiEgQV9@ep-noisy-surf-36032677-pooler.us-east-2.aws.neon.tech/todo2?sslmode=require"
+# print()
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
